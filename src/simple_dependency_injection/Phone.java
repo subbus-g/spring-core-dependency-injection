1b98e163a5d0e8b77e443f9a0534aa53a0d112c2
subbus <subbareddyg83@gmail.com>
2020-11-19 15:57:22 +0530

    get object from spring ioc container instead of manually creating it

--- src/simple_dependency_injection/Phone.java
+++ src/simple_dependency_injection/Phone.java
@@ -8,17 +8,20 @@ public class Phone
 {
     public static void main(String[] args) 
     {
-        //load the spring configaration file
-        ApplicationContext context = new ClassPathXmlApplicationContext("./simple_dependency_injection/config.xml");
+        // load the spring configaration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "./simple_dependency_injection/config.xml");
         System.out.println("xml configuration is loaded successfully");
 
-        Sim sim = new Airtel();
+        // retrive the bean from spring container
+        Sim sim = context.getBean("sim", Sim.class);
+
+        // call the methods on the bean
         sim.call();
         sim.turnOnInternet();
 
-        sim = new Jio();
-        sim.call();
-        sim.turnOnInternet();
+        // close the context
+        context.close();
 
     }
 }
