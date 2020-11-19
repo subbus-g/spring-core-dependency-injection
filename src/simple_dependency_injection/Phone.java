78753f6cfcf9c4cee1eef68b351a8c4fb6a4fcd2
subbus <subbareddyg83@gmail.com>
2020-11-19 15:35:10 +0530

    add spring dependency files and load the configaration the file

--- src/simple_dependency_injection/Phone.java
+++ src/simple_dependency_injection/Phone.java
@@ -1,8 +1,17 @@
 package simple_dependency_injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class Phone 
 {
     public static void main(String[] args) 
     {
+        //load the spring configaration file
+        ApplicationContext context = new ClassPathXmlApplicationContext("./simple_dependency_injection/config.xml");
+        System.out.println("xml configuration is loaded successfully");
+
         Sim sim = new Airtel();
         sim.call();
         sim.turnOnInternet();
@@ -10,5 +19,6 @@ public class Phone
         sim = new Jio();
         sim.call();
         sim.turnOnInternet();
+
     }
 }
